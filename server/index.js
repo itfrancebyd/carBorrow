24c3b4ca9bfdd651262733d8e0d71f5ce074e387
bydfranceit <bydfrance.it@gmail.com>
2025-08-19 14:34:41 +0200

    save

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 const express = require('express')
 const app = express()
+const port = '3001'
+
+app.get('/', (req, res) => {
+    res.send('test get')
+})
+
+app.listen(port, () => {
+    console.log("server runs on the port" + port)
+})
