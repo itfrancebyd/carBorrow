47550377017d9be7b9cbef01da379321e8930a71
yunbo-z <zyunbo0330@gmail.com>
2025-08-28 17:23:57 +0200

    feat: init database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+// const express = require('express')
+import express from 'express'
+import 'dotenv/config'
+
 const app = express()
 const port = '3001'
+const env_data = process.env
+
+import { createClient } from '@supabase/supabase-js'
+// Create a single supabase client for interacting with your database
+const supabase = createClient(env_data.SUPABASE_URL, env_data.SUPABASE_PUBLISHABLE_KEY)
+
+const {data,error} = await supabase.from('car fleet').select()
+console.log(data)
 
 app.get('/', (req, res) => {
     res.send('test get')
 })
 
 app.listen(port, () => {
-    console.log("server runs on the port" + port)
+    console.log("server runs on the port: " + port)
 })
