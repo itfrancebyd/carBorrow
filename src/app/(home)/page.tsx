903e485cd6d834e650930647bf5ebb5074f3ffb6
yunbo-z <zyunbo0330@gmail.com>
2025-10-08 18:04:18 +0200

    chore: add filterVehicle

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import TableGrid from "../components/forms/tableGrid";
-import vehicleContent from "@/docs/vehicle_info.json"
 import SubTitle from "../components/subTitle";
-import Filter from "../components/filter";
 import { useEffect, useState } from "react";
 import modelInfoJson from "@/docs/modelInfo.json"
 import DataMeasure from "@/app/components/dataMeasure"
 import { createClient } from "@/utils/supabase/client";
 import VehiclePopModal from "../components/forms/vehiclePopModal";
+import FilterVehicle from "../components/filterVehicle";
 
 interface carFleet {
   id: string;
@@ -176,6 +175,21 @@ export default function Home() {
     { key: "version_name", label: "Version Name" },
     { key: "status", label: "Status" }
   ]
+  const filterTitle = [
+    { key: "vin", label: "VIN" },
+    { key: "plate_number", label: "Plate Number" },
+    { key: "model_name", label: "Model Name" },
+    { key: "version_name", label: "Version Name" },
+    { key: "interior_colour", label: "Interior Colour" },
+    { key: "exterior_colour", label: "Exterior Colour" },
+    { key: "km", label: "Km" },
+    { key: "battery", label: "Battery" },
+    { key: "usage_update_date", label: "Usage Update Date" },
+    { key: "key_1", label: "Key 1" },
+    { key: "key_2", label: "Key 2" },
+    { key: "current_location", label: "Current Location" },
+    { key: "status", label: "Status" }
+  ]
   const popupWindowInfo = [
     { key: "id", label: "Vehicle Id" },
     { key: "vin", label: "VIN" },
@@ -198,7 +212,7 @@ export default function Home() {
     <div className="flex flex-col h-screen">
       <SubTitle subTitleName="Vehicles"></SubTitle>
       {/* <DataMeasure></DataMeasure> */}
-      <Filter setFilterInfo={setVehicleInfo} selectInfo={modelInfo} filterItems={tableTitle}></Filter>
+      <FilterVehicle setFilterInfo={setVehicleInfo} selectInfo={modelInfo} filterItems={filterTitle}></FilterVehicle>
       <div className="flex-1">
         <TableGrid
           formTitle="Vehicle"
